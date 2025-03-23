0545a6d6c253685fbb591a4460bac72c3acf1d82
LUNGISAN2 <lungisanil@discovery.co.za>
2025-03-23 15:13:35 +0200

    Fix swagger load issue

--- AnagramServiceWeb/src/main/java/co/za/bsg/web/controller/WordController.java
+++ AnagramServiceWeb/src/main/java/co/za/bsg/web/controller/WordController.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Work-Controller", description = "Work Controller")
+@Tag(name = "Word-Controller", description = "Work Controller")
 @RestController
 @RequestMapping("/word")
 public class WordController {
